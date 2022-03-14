8ad055d9f061ccb03cbfff8110278ac901bcfd7d
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-14 10:04:43 +0700

    Update queue.py

--- Music/MusicUtilities/database/queue.py
+++ Music/MusicUtilities/database/queue.py
@@ -1,10 +1,13 @@
 from typing import Dict, List, Union
+
 from Music import db
 
 pytgdb = db.pytg
 admindb = db.admin
 
-## Queue Chats
+
+## Queue Chats Audio
+
 
 async def get_active_chats() -> list:
     chats = pytgdb.find({"chat_id": {"$lt": 0}})
@@ -14,18 +17,21 @@ async def get_active_chats() -> list:
     for chat in await chats.to_list(length=1000000000):
         chats_list.append(chat)
     return chats_list
-    
+
+
 async def is_active_chat(chat_id: int) -> bool:
     chat = await pytgdb.find_one({"chat_id": chat_id})
     if not chat:
         return False
     return True
 
+
 async def add_active_chat(chat_id: int):
     is_served = await is_active_chat(chat_id)
     if is_served:
         return
     return await pytgdb.insert_one({"chat_id": chat_id})
+
 
 async def remove_active_chat(chat_id: int):
     is_served = await is_active_chat(chat_id)
